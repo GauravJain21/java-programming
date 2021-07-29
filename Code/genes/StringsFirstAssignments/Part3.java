a2e6144c1436504f74df937fb95796fb6dc48085
Gaurav Jain <gauravkjain12345@gmail.com>
2021-07-29 19:33:31 +0530

    Add remaining Part 3

--- Code/genes/StringsFirstAssignments/Part3.java
+++ Code/genes/StringsFirstAssignments/Part3.java
@@ -10,12 +10,26 @@ public class Part3 {
         return secondIndex != -1;
     }
     public void testing() {
-        String strings[][] = {{"a", "aaa"}, {"b", "vbsdfb"}, {"a", "a"}};
+        String strings[][] = {{"a", "aaa"}, {"b", "vbsdfb"}, {"a", "a"}, {"ABC", "ACB"}};
+        System.out.println("Testing twoOccurences");
         for(String[] ab: strings) {
             System.out.println(ab[0] + "occurs twice in " + ab[1] + " : " + twoOccurences(ab[0], ab[1]));
         }
+        System.out.println("Testing lastPart");
+        for(String[] ab: strings) {
+            System.out.println("Last Part of " + ab[1] + " with " + ab[0] + " as starting = " + lastPart(ab[0], ab[1]));
+        }
         return;
     }
+
+    public String lastPart(String stringa, String stringb) {
+        int startIndex = stringb.indexOf(stringa);
+        if(startIndex == -1) {
+            return stringb;
+        }
+        return stringb.substring(startIndex);
+    }
+
     public static void main(String[] args) {
         Part3 part3 = new Part3();
         part3.testing();
