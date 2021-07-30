662bc4f6c4899a4fb71d57b0408f7b5f858f0abd
Gaurav Jain <gauravkjain12345@gmail.com>
2021-07-30 18:34:42 +0530

    Add Week2 Assignment2 Part3 and All genes example

---
+++ Code/genes/AllGenesStored.java
@@ -0,0 +1,57 @@
+import edu.duke.StorageResource;
+
+public class AllGenesStored {
+
+    public int findStopCodon(String dna, int startIndex, String stopCodon) {
+        int stopCodonIndex = dna.indexOf(stopCodon, startIndex + 3);
+        return stopCodonIndex == -1? dna.length(): stopCodonIndex;
+    }
+    public int findSmallestStopCodon(String dna, int startIndex) {
+        return Math.min(findStopCodon(dna, startIndex, "TAA"),
+                Math.min(findStopCodon(dna, startIndex, "TAG"),
+                        findStopCodon(dna, startIndex, "TGA")));
+    }
+
+    public String findGene(String dna, int startIndex) {
+        startIndex = dna.indexOf("ATG", startIndex);
+        if(startIndex == -1) {
+            return "";
+        }
+        int endIndex =  findSmallestStopCodon(dna, startIndex);
+        while (endIndex != dna.length() && (endIndex - startIndex)%3 != 0) {
+            endIndex = findSmallestStopCodon(dna, endIndex + 3);
+        }
+        if(endIndex == dna.length()) {
+            return "";
+        }
+        return dna.substring(startIndex, endIndex + 3);
+    }
+    public StorageResource getAllGenes(String dna) {
+        int startIndex = dna.indexOf("ATG");
+        StorageResource geneList = new StorageResource();
+        while (true) {
+            String currentGene = findGene(dna, startIndex);
+            if(currentGene.isEmpty()) {
+                break;
+            }
+            geneList.add(currentGene);
+            startIndex += currentGene.length();
+        }
+        return geneList;
+    }
+    public void testGetAllGene() {
+        String[] dnas = {"AATGAAATAATATGGT", "ATG", "TAA", "AGGT", "ATGTAA", "gatgctataat", "ATGTAATGATAG", "ATGTAAATGTAGATGTGA"};
+        for (String dna : dnas) {
+            System.out.println("DNA strand is: " + dna);
+            StorageResource geneList = getAllGenes(dna);
+            for (String gene : geneList.data()) {
+                System.out.println("Gene is: " + gene);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        AllGenesStored allGenesStored = new AllGenesStored();
+        allGenesStored.testGetAllGene();
+    }
+
+}
--- Code/genes/StringsSecondAssignments/Part3.java
+++ Code/genes/StringsSecondAssignments/Part3.java
@@ -35,9 +35,28 @@ public class Part3 {
             System.out.println("Genes in " + dna + ": " + countAllGenes(dna));
         }
     }
+    public void findAbc(String input){
+        int index = input.indexOf("abc");
+        while (true){
+            if (index == -1 || index >= input.length() - 3){
+                break;
+            }
+            //System.out.println(index);
+            String found = input.substring(index+1, index+4);
+            System.out.print(found + ",");
+            index = input.indexOf("abc",index+3);
+            //System.out.println("After updating" + index);
+        }
+    }
+
+    public void test(){
+        //findAbc("abcd");
+        findAbc("abcabcabcabca");
+    }
     public static void main(String[] args) {
         Part3 part3 = new Part3();
         part3.testCountGenes();
+        part3.test();
     }
 
 }
