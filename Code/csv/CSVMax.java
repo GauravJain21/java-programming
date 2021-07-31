1fcbd379e530e96c4169b1a75152bdf298321ad7
Gaurav Jain <gauravkjain12345@gmail.com>
2021-07-31 16:11:20 +0530

    Add Many days max logic

--- Code/csv/CSVMax.java
+++ Code/csv/CSVMax.java
@@ -1,6 +1,9 @@
+import edu.duke.DirectoryResource;
 import edu.duke.FileResource;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
+
+import java.io.File;
 
 public class CSVMax {
     public CSVRecord hottestHourInFile(CSVParser parser) {
@@ -16,10 +19,27 @@ public class CSVMax {
         return maxRecord;
     }
 
+    public CSVRecord hottestHourInManyDays() {
+        DirectoryResource dr = new DirectoryResource();
+        CSVRecord hottestHour = null;
+        for (File f : dr.selectedFiles()) {
+            FileResource fr = new FileResource(f.getAbsolutePath());
+            CSVParser parser = fr.getCSVParser();
+            CSVRecord currHottestHour = hottestHourInFile(parser);
+            double currentTemp = Double.parseDouble(currHottestHour.get("TemperatureF"));
+            if(hottestHour == null || currentTemp > Double.parseDouble(hottestHour.get("TemperatureF"))) {
+                hottestHour = currHottestHour;
+            }
+        }
+        return hottestHour;
+    }
+
     public void tester() {
-        FileResource fr = new FileResource();
-        CSVParser parser = fr.getCSVParser();
-        System.out.println(hottestHourInFile(parser));
+        //FileResource fr = new FileResource();
+        //CSVParser parser = fr.getCSVParser();
+        //System.out.println(hottestHourInFile(parser));
+        System.out.println(hottestHourInManyDays());
+
     }
     public static void main(String[] args) {
         CSVMax obj = new CSVMax();
